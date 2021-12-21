d1ba564eeca8dbc117a3c3f43ed3d82dcb802480
Ahmed Mgua <mguaahmed@gmail.com>
2021-12-21 09:42:54 +0300

    Initial Commit

---
+++ OpticalCharacterRecognition/OpticalCharacterRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpticalCharacterRecognitionApp.swift
+//  OpticalCharacterRecognition
+//
+//  Created by Ahmed Mgua on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct OpticalCharacterRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
